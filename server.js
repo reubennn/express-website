fe28891d78f76b2858e305422b2b47a28c3bf4b4
Reuben Smith <reuben-smith@hotmail.com>
2020-07-16 16:24:41 +0930

    Moves static directory from provided and sets up basic Express Website

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+const PORT = 3000;
+
+app.use(express.static(path.join(__dirname, "./static")));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "./static/index.html"));
+});
+
+app.get("/speakers", (req, res) => {
+    res.sendFile(path.join(__dirname, "./static/speakers.html"));
+});
+
+app.listen(PORT, () => {
+    console.log(`Express server listening on PORT ${PORT}!`);
+});
